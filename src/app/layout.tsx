16f9b453fb790b9ebece9a3344534ac6c6dec38c
Seungrok <sonicrok93@gmail.com>
2023-01-27 05:09:17 +0900

    fix: resolve eslint error message
    no-undef eslint rule for react import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css'
+import { ReactNode } from 'react'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       {/*
