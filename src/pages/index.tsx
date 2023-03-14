5c915248b4f57f8c22b3d8bbf14282f5ff883128
Seungrok <sonicrok93@gmail.com>
2023-03-15 05:47:07 +0900

    refactor: move duplicated layouts to  Template component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React, { FunctionComponent, useMemo } from 'react'
-import GlobalStyle from 'components/Common/GlobalStyle'
-import styled from '@emotion/styled'
 import Introduction from 'components/Main/Introduction'
 import CategoryList, { CategoryListProps } from 'components/Main/CategoryList'
 import PostList from 'components/Main/PostList'
-import Footer from 'components/Common/Footer'
 import { graphql } from 'gatsby'
 import { PostListItemType } from 'types/PostItem.types'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
 import queryString, { ParsedQuery } from 'query-string'
+import Template from 'components/Common/Template'
 
 type IndexPageProps = {
   location: {
@@ -25,12 +23,6 @@ type IndexPageProps = {
     }
   }
 }
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`
 
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
   location: { search },
@@ -69,16 +61,14 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
   )
 
   return (
-    <Container>
-      <GlobalStyle />
+    <Template>
       <Introduction profileImage={gatsbyImageData} />
       <CategoryList
         selectedCategory={selectedCategory}
         categoryList={categoryList}
       />
       <PostList selectedCategory={selectedCategory} posts={edges} />
-      <Footer />
-    </Container>
+    </Template>
   )
 }
 
--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,9 +1,35 @@
+import Template from 'components/Common/Template'
+import { graphql } from 'gatsby'
 import { FunctionComponent } from 'react'
 
 type PostTemplateProps = {}
 
 const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  return <div>Post Template</div>
+  console.log(props)
+  return <Template>Post Template</Template>
 }
 
 export default PostTemplate
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
