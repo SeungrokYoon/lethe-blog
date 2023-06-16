b7aa4dbbdfd494a46c49ef97979a17e0aeb75a86
Seungrok <sonicrok93@gmail.com>
2023-06-16 16:18:22 +0900

    feat: change global font to Poppins

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -2,13 +2,13 @@ import { css, Global } from '@emotion/react'
 import { FunctionComponent } from 'react'
 
 const defaultStyle = css`
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&family=Poppins:wght@400;500;700&display=swap');
 
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Nanum Myeongjo', serif;
+    font-family: 'Poppins', 'Nanum Myeongjo', serif;
     scroll-behavior: smooth;
   }
 
